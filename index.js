7fcf641d1428b32870fbc81088b2d330998f63b0
JacksonJ97 <jacksonjude97@gmail.com>
2021-05-23 20:21:48 -0400

    implemented the function computer play with less code

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 function computerPlay() {
-  const num = Math.floor(Math.random() * 3);
-  let choice = "";
-
-  if (num === 0) {
-    choice = "rock";
-  } else if (num === 1) {
-    choice = "paper";
-  } else {
-    choice = "scissors";
-  }
-  console.log(choice);
-  return choice;
+  const choice = ["rock", "paper", "scissors"];
+  return choice[Math.floor(Math.random() * 3)];
 }
 
 computerPlay();
+
+// function playRound(playerSelection, computerSelection) {
+//   let result = "";
+
+//   if (playerSelection === "rock" && computerSelection === "rock") {
+//     result = "Draw";
+//   }
+//   if (playerSelection === "rock" && computerSelection === "paper") {
+//     result = "You lose";
+//   }
+//   if (playerSelection === "rock" && computerSelection === "scissors") {
+//     result = "You win";
+//   }
+//   if (playerSelection === "paper" && computerSelection === "rock") {
+//     result = "You win";
+//   }
+//   if (playerSelection === "paper" && computerSelection === "paper") {
+//     result = "Draw";
+//   }
+//   if (playerSelection === "paper" && computerSelection === "scissors") {
+//     result = "You lose";
+//   }
+//   if (playerSelection === "scissors" && computerSelection === "rock") {
+//     result = "You lose";
+//   }
+//   if (playerSelection === "scissors" && computerSelection === "paper") {
+//     result = "You win";
+//   }
+//   if (playerSelection === "scissors" && computerSelection === "scissors") {
+//     result = "Draw";
+//   }
+//   console.log(computerSelection);
+//   return result;
+// }
+
+// const playerSelection = "rock";
+// console.log(playRound(playerSelection, computerPlay()));
